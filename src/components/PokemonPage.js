5cb9f147e7062cac49c0bb1f700ead4cf715352c
Angela Palaszewski <delarosa.ang@gmail.com>
2023-04-18 23:58:41 -0600

    complete lab?

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemons, setPokemons] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then((resp) => resp.json())
+    .then((data) => setPokemons(data))
+  }, [])
+
+  const displayPokemons = pokemons.filter((pokemon) => {
+    return pokemon.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+  function addPokemon(newPokemon) {
+    const updatedPokemon = [...pokemons, newPokemon]
+    setPokemons(updatedPokemon);
+  }
+  
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={addPokemon}/>
       <br />
-      <Search />
+      <Search search={search} onSearch={setSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={displayPokemons}/>
     </Container>
   );
 }
